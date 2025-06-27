8d29ebbfe314707e3cca714ceb5fe769295f016a
sudlon <sudlondon@outlook.com>
2025-06-27 17:39:52 +0200

    Add appsettings also to json file + clarifying comment at DeduplicateLinebreaks method

--- src/OneNoteMdExporter/Services/ConverterService.cs
+++ src/OneNoteMdExporter/Services/ConverterService.cs
@@ -193,6 +193,7 @@ namespace alxnbl.OneNoteMdExporter.Services
         {
             // PanDoc seems to produce 2 linebreaks characters for each linebreak in original DocX file
             // Replace all pair of linebreak by a single linebreak
+            // NB: This doesn't capture the extra line breaks between indented items because there is a ">" between the line breaks..
             var pageTxtModified = Regex.Replace(pageTxt, @"(\n{2}|\r{2}|(\r\n){2})", Environment.NewLine);
 
             return pageTxtModified;
